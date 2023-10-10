ea54ae2d6a6fa143592dc30b26037df7d20ad588
testgithuburt <132937040+testgithuburt@users.noreply.github.com>
2023-10-10 10:58:24 +0200

    Create vpc.tf

---
+++ vpc.tf
@@ -0,0 +1,11 @@
+# VPC Creation using CIDR block available in vars.tf
+
+resource "aws_vpc" "provisionerVPC"{
+    cidr_block = var.vpc_cidr
+    enable_dns_hostnames=true
+    enable_dns_support = true
+
+    tags = {
+        Name = "dev-terraform-vpc"
+    }
+}
