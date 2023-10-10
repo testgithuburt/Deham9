caa8f3cca45388446f551a013467528b86411ae9
naveena veluru <naveenaveluru2@gmail.com>
2023-10-10 11:17:21 +0200

    Hello naveena

--- vpc.tf
+++ vpc.tf
@@ -1,5 +1,5 @@
 # VPC Creation using CIDR block available in vars.tf
-
+# VPC Creation using CIDR block available in vars.tf counting objects
 resource "aws_vpc" "provisionerVPC"{
     cidr_block = var.vpc_cidr
     enable_dns_hostnames=true
