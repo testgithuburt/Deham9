b61abc1bba393fffe987d39e900c59a50389068d
naveena veluru <naveenaveluru2@gmail.com>
2023-10-24 17:52:09 +0200

    addingnewfiles

--- main.tf
+++ main.tf
@@ -1,221 +1,129 @@
-terraform {
-  required_providers {
-    aws = {
-      source  = "hashicorp/aws"
-      version = "~> 3.27"
+# resource "aws_instance" "ec2_instance" {
+#   ami=var.AMIS[var.AWS_REGION] 
+#   instance_type = "t2.micro"
+#   associate_public_ip_address = true
+#   tags = {
+#     Name = "Terraform_test_ec2"
+#   }
+# }
+# output public_ip{
+#   value=aws_instance.ec2_instance.public_ip
+# }
+# resource "aws_instance" "webserver" {
+#   for_each = var.webservers
+#     ami=var.AMIS[var.AWS_REGION] 
+#     instance_type = "t2.micro"
+#     associate_public_ip_address = true
+#     tags = {
+#       Name = each.value["name"]
+#     }
+# }
+data "aws_availability_zones" "available"{}
+# VPC Creation using CIDR block available in vars.tf
+resource "aws_vpc" "provisionerVPC"{
+    cidr_block = var.vpc_cidr
+    enable_dns_hostnames=true
+    enable_dns_support = true
+    tags = {
+        Name = "dev-terraform-vpc"
     }
-  }
-
-  required_version = ">= 0.14.9"
 }
-
-
-#Provider profile and region in which all the resources will create
-provider "aws" {
-  profile = "default"
-  region  = "us-west-2"
+# Public subnet public CIDR block available in vars.tf and provisionersVPC
+resource "aws_subnet" "public_subnet"{
+    cidr_block = var.public_cidr
+    vpc_id = aws_vpc.provisionerVPC.id
+    map_public_ip_on_launch = true
+    availability_zone = data.aws_availability_zones.available.names[1]
+    tags = {
+        Name = "dev-public-subnet"
+    }    
 }
-
-resource "aws_vpc" "my_vpc" {
-  cidr_block = "10.0.0.0/16"  
-
-  tags = {
-    Name = "my-vpc"
-  }
-}
-# creating public and private subnets 
-resource "aws_subnet" "public_subnet" {
-  vpc_id     = aws_vpc.my_vpc.id
-  cidr_block = "10.0.1.0/24"
-  availability_zone = "us-west-2a"  
-
-  map_public_ip_on_launch = true
-
-  tags = {
-    Name = "public_subnet"
-  }
-}
-
-resource "aws_subnet" "private_subnet" {
-  vpc_id     = aws_vpc.my_vpc.id
-  cidr_block = "10.0.2.0/24"
-  availability_zone = "us-west-2b"  
-
-  map_public_ip_on_launch = false
-
-  tags = {
-    Name = "private_subnet"
-  }
-}
-
+# To access EC2 instance inside a virtual private cloud we need an Internet Gateway and a routing table connecting the subnetto the Internet Gateway
 # Creating Internet Gateway
-
-resource "aws_internet_gateway" "my_gw" {
-  vpc_id = aws_vpc.my_vpc.id
-
-  tags = {
-    Name = "my_gw"
-  }
-} 
+resource "aws_internet_gateway" "gw"{
+    vpc_id = aws_vpc.provisionerVPC.id
+    tags = {
+        Name = "dev-gw"
+    }
+}
 # Public Route Table
 resource "aws_route_table" "public_route"{
-    vpc_id = aws_vpc.my_vpc.id
-
+    vpc_id = aws_vpc.provisionerVPC.id
     route {
-        cidr_block = "10.0.1.0/24"
-        gateway_id = aws_internet_gateway.my_gw.id
+        cidr_block = var.cidr_blocks
+        gateway_id = aws_internet_gateway.gw.id
     }
     tags = {
-      Name = "public-route"  
+      Name = "dev-public-route"  
     }
 }
 # Associate Public Subnet with Public Route Table
-resource "aws_route_table_association" "public_subnet_route" {
-  subnet_id      = aws_subnet.public_subnet.id
-  route_table_id = aws_route_table.public_route.id
-}
-
-# Private Route Table
-resource "aws_route_table" "private_route"{
-    vpc_id = aws_vpc.my_vpc.id
-
-    route {
-        cidr_block = "10.0.2.0/24"
-        gateway_id = aws_internet_gateway.my_gw.id
-    }
-    tags = {
-      Name = "private-route"  
-    }
-}
-
-# Associate Private Subnet with Private Route Table
-resource "aws_route_table_association" "private_subnet_route" {
-  subnet_id      = aws_subnet.private_subnet.id
-  route_table_id = aws_route_table.private_route.id
-}
-# NACL 
-resource "aws_network_acl" "my_network_acl" {
-  vpc_id = aws_vpc.my_vpc.id
-}
-
-resource "aws_network_acl_rule" "my_network_acl" {
-  network_acl_id = aws_network_acl.my_network_acl.id
-  rule_number    = 200
-  egress         = false
-  protocol       = "tcp"
-  rule_action    = "allow"
-  cidr_block     = aws_vpc.my_vpc.cidr_block
-  from_port      = 22
-  to_port        = 22
+resource "aws_route_table_association" "public_subnet_assoc"{
+    count = 1
+    route_table_id = aws_route_table.public_route.id
+    subnet_id = aws_subnet.public_subnet.id
+    depends_on = [aws_route_table.public_route,aws_subnet.public_subnet]    
 }
 # Security Group Creation for provisionerVPC
-resource "aws_security_group" "allow_ssh_http" {
-  name        = "allow_ssh_http"
-  description = "Allow SSH and http inbound traffic"
-  vpc_id      = aws_vpc.my_vpc.id
-
-
-  # Ingress Security Port 22 (Inbound)
-  ingress {
-    description      = "SSH from VPC"
-    from_port        = 22
-    to_port          = 22
-    protocol         = "tcp"
-    cidr_blocks      = [aws_vpc.my_vpc.cidr_block]
-  }
-  # Ingress Security Port 80 (Inbound)
-  ingress {
-    description      = "HTTP from VPC"
-    from_port        = 80
-    to_port          = 80
-    protocol         = "tcp"
-    cidr_blocks      = [aws_vpc.my_vpc.cidr_block]
-  }
-  # All egress/outbound Access
-  egress {
-    from_port        = 0
-    to_port          = 0
-    protocol         = "-1"
-    cidr_blocks      = ["0.0.0.0/0"]
-  }
-
-  tags = {
-    Name = "allow_tcp"
-  }
+resource "aws_security_group" "dev_terraform_sg_allow_ssh_http"{
+    name="dev-sg"
+    vpc_id = aws_vpc.provisionerVPC.id
 }
-
-#ec2 creating
-# terraform {
-#   required_providers {
-#     aws = {
-#       source  = "hashicorp/aws"
-#       version = "~> 3.27"
-#     }
-#   }
-
-#   required_version = ">= 0.14.9"
-# }
-
-#Provider profile and region in which all the resources will create
-# provider "aws" {
-#   profile = "default"
-#   region  = "us-west-2"
-# }
-resource "aws_instance" "web_server" {
-  ami           = "ami-0e2e9c570f999a4c8" # This is an example Amazon Linux AMI ID. Change it according to your requirements.
-  instance_type = "t2.micro"
-  key_name      = "web-key"   
-
-  tags = {
-    Name = "my-web"
-  }
+# Ingress Security Port 22 (Inbound)
+resource "aws_security_group_rule" "ssh_ingress_access"{
+    from_port = 22
+    protocol = "tcp"
+    security_group_id = aws_security_group.dev_terraform_sg_allow_ssh_http.id
+    to_port = 22
+    type = "ingress"
+    cidr_blocks = [var.cidr_blocks]
 }
-
-resource "aws_subnet" "my_subnet" {
-  vpc_id     = aws_vpc.my_vpc.id
-  cidr_block = "10.0.1.0/24"
-
-  tags = {
-    Name = "my-subnet"
-  }
+# Ingress Security Port 80 (Inbound)
+resource "aws_security_group_rule" "http_ingress_access"{
+    from_port = 80
+    protocol = "tcp"
+    security_group_id = aws_security_group.dev_terraform_sg_allow_ssh_http.id
+    to_port = 80
+    type = "ingress"
+    cidr_blocks = [var.cidr_blocks]
 }
-
-
-# resource "aws_subnet" "my_vpc" {
-#   vpc_id     = aws_vpc.my_vpc.id
-#   cidr_block = "10.0.1.0/24"
-
-#   tags = {
-#     Name = "my-subnet"
-#   }
-# }
-
-# creating S3 bucket
-
-# terraform {
-#   required_providers {
-#     aws = {
-#       source  = "hashicorp/aws"
-#       version = "~> 3.27"
-#     }
-#   }
-
-#   required_version = ">= 0.14.9"
-# }
-
-#  # #Provider profile and region in which all the resources will create
-#  provider "aws" {
-#    profile = "default"
-#    region  = "us-west-2"
-#  }
-
-# #Resource to create s3 bucket
-# resource "aws_s3_bucket" "neno-bucket" {
-#   bucket = "nb-neno-bucket"
-#    tags = {
-#     Name        = "My bucket"
-#     Environment = "Dev"
-#   }
-
-# }
-
+# All egress/outbound Access
+resource "aws_security_group_rule" "all_egress_access"{
+    from_port = 0
+    protocol = "-1"
+    security_group_id = aws_security_group.dev_terraform_sg_allow_ssh_http.id
+    to_port = 0
+    type = "egress"
+    cidr_blocks = [var.cidr_blocks]
+}
+# Instance Configuration
+resource "aws_instance" "provisioner-remoteVM"{
+    ami = "ami-0e2e9c570f999a4c8"
+    instance_type = var.instance_type
+    key_name = "app"
+    vpc_security_group_ids = [aws_security_group.dev_terraform_sg_allow_ssh_http.id]
+    subnet_id = aws_subnet.public_subnet.id
+    tags = {
+        Name = "remote-instance"
+    }
+    provisioner "remote-exec"{
+        inline = [
+            "sudo yum update -y",
+            "sudo yum install -y nginx",
+            "sudo service nginx start"
+        ]
+        on_failure = continue
+    }
+    provisioner "local-exec"{
+        #ami=data.aws_ami.packeramis.id
+        #instance_type="t2.micro"
+        #when = "destroy"
+        command = "echo Instance Type=${self.instance_type},Instance ID=${self.id},Public DNS=${self.public_dns},AMI ID=${self.ami} >> allinstancedetails"
+    }
+    connection {
+        type = "ssh"
+        host = aws_instance.provisioner-remoteVM.public_ip
+        user = "ec2-user"
+        private_key = file("${path.module}/app.pem")
+    }
+}
--- var.tf
+++ var.tf
@@ -1,3 +1,53 @@
+variable "AWS_ACCESS_KEY"{
+    description="ASIA5GNN7GIUSF62TN43"
+}
+variable "AWS_SECRET_KEY"{
+    description="Kye0lI43777XEd2BBQF8OYDYLry/7vmQ9YwL/bZB"
+}
+variable "AWS_REGION"{
+    default="us-west-2"
+    description="us-west-2"
+}
+# Network Mask - 255.255.255.0 Addresses Available - 256
 variable "vpc_cidr"{
     default = "10.0.1.0/24"
+}
+variable "public_cidr"{
+    default = "10.0.1.0/28"
+}
+variable "private_cidr"{
+    default = "10.0.1.16/28"
+}
+variable "cidr_blocks"{
+    default = "0.0.0.0/0"
+}
+variable "instance_type"{
+    default = "t3.micro"
+}
+variable "apac_region"{
+    default="us-west-2"
+}
+variable "AMIS"{
+    type = map(string)
+    description= "Region specific AWS Machine Images (AMI)"
+    default={
+        us-east-1 = "ami-0d2017e886fc2c0ab"
+        us-west-2 = "ami-0d2017e886fc2c0ab"
+    }
+    #validation {
+    #    condition = length(var.AMIS) > 4 && substr(var.AMIS["us-west-2"],0,4)=="ami-"
+    #    error_message = "The image_id value must be a valid AMI id, starting with \"ami-\"."
+    #}
+}
+variable "webservers"{
+    type = map(map(string))
+    description = "AWS EC2 webserver instance with names WEBAPI-Server and Web-App-Server"
+    default = {
+      "webserver1" = {
+        "name" = "WebAPI-Server"
+      }
+    #   "webserver2" = {
+    #     "name" = "WebApp-Server"
+    #   }
+    }
 }
